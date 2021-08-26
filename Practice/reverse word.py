22f7d2e885d189857fa4faf30c4244aab35d0b78
@Amit0417 <r.amit.mahour@gmail.com>
2021-08-26 20:41:51 +0530

    first commit

---
+++ Practice/reverse word.py
@@ -0,0 +1,20 @@
+# reverse the word  in the given string without using function
+string = "geeks quiz practice code"
+s1 = string.split()
+s2 = s1[::-1]
+out = " ".join(s2)
+print(out)
+
+
+# reverse the word in given string using function
+def rev(string_1):
+    words = string_1.split(' ')
+    rev_string = " ".join(reversed(words))
+    return rev_string
+
+
+if __name__ == "__main__":
+    str1 = "geeks quiz practice code"
+    str2 = input("enter your string")
+    print(rev(str1))
+    print(rev(str2))
